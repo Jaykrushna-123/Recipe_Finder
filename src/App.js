37e15e1292c53c5d47f6939d9c0ea21389a5172f
jay <jaykrushnadas04@gmail.com>
2021-03-11 11:23:24 +0530

    i am pushing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect,useState,useRef} from 'react';
 
-function App() {
+let App = () => {
+  let [ingredientList,updateIngredientList] = useState([]); 
+  let [loading,setLoading] = useState(false);
+
+  let inputRef = useRef(null);
+  let Api_Key= 'b819919ea5e0bae85884f17b9f048333';
+  let Api_Id= '27e25c04';  
+
+   let search =() => {
+     searchForRecipe(inputRef.current.value);
+     inputRef.current.value ="";
+   }
+
+
+  let searchForRecipe =(query) =>{
+    setLoading(true);
+    let url= `search?q=${query}&app_id=${Api_Id}&app_key=${Api_Key} `;
+    fetch(url,{mode:"no-cors"})
+    .then(response =>{
+      return response.json();
+    }).then(res =>{
+      console.log(res.hits);
+      updateIngredientList(res.hits);
+      setLoading(false);
+    }).catch(err =>{
+      console.log(err);
+      setLoading(false);
+    })
+  }
+
+  useEffect(()=>{
+ searchForRecipe('Chicken')
+  },[]); 
+ 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="InputWrapper">
+          <input ref={inputRef}  placeholder="Search for recipe" />
+          <button   onClick={search} >Search</button>
+        </div>
+        {loading && <p>Loading...</p>}
+    <div className="Wrapper">
+     {ingredientList.map(({recipe})=>{
+       let {label,image,ingredientLines} = recipe;
+       return (
+         <div  key={label} className="Ingredient">
+               <span>{label}</span>
+               <img src={image}  alt="photos"  />
+               <div className="Steps">
+               {ingredientLines.map((step,index) =>{
+                 return <p key={index }>{step}</p>
+               })}
+               </div>
+         </div>
+       );
+     })}
+    </div>
       </header>
     </div>
   );
